f10efbd150041aeeffd8a19b8f2691f9360b2000
wizardcos <taharajpoot204@gmail.com>
2024-11-09 13:26:25 +0500

    basic setup

--- INSTA/settings.py
+++ INSTA/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crud.app.CrudConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
